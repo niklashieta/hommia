39e6481f4555006dc310eb906b04a68753af33ed
k9279 <k9279@student.jamk.fi>
2018-02-12 15:21:21 +0200

    aa

--- hommia/Program.cs
+++ hommia/Program.cs
@@ -26,7 +26,7 @@ namespace Collections
                 }
             } while (result);
             for(int i=0;i<list.Count;i++)
-            {
+            {//one
                 int number = list[i];
                 if (i == list.Count - 1)
                 {
